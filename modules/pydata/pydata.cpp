41adabf7af005d5dbf834e5cd9d293970bb5989c
Gunnar Läthén <gunnar@lathen.se>
2016-09-09 20:33:09 +0200

    Extended set_image to support more data formats and number of components

--- modules/pydata/pydata.cpp
+++ modules/pydata/pydata.cpp
@@ -25,19 +25,55 @@ T* getProcessor(std::string identifer) {
     return typedProcessor;
 }
 
+NumericType getNumericType(std::string format) {
+    
+    if (format == py::format_descriptor<float>::format() ||
+        format == py::format_descriptor<double>::format())
+        return NumericType::Float;
+    else if (format == py::format_descriptor<char>::format() ||
+        format == py::format_descriptor<short>::format() ||
+        format == py::format_descriptor<int>::format() ||
+        format == py::format_descriptor<long int>::format())
+        return NumericType::SignedInteger;
+    else if (format == py::format_descriptor<unsigned char>::format() ||
+        format == py::format_descriptor<unsigned short>::format() ||
+        format == py::format_descriptor<unsigned int>::format() ||
+        format == py::format_descriptor<unsigned long int>::format())
+        return NumericType::UnsignedInteger;
+    else
+        throw std::runtime_error("Data type not supported");
+}
+
 void set_image(std::string processorIdentifier, py::buffer b) {
     auto imageSource = getProcessor<ImageSourceBuffer>(processorIdentifier);
 
     py::buffer_info info = b.request();
-    if (info.ndim != 2)
-        throw std::runtime_error("Incompatible buffer dimension");
 
+    // Determine the numeric type
+    NumericType type = getNumericType(info.format);
+
+    // Determine the number of components
+    size_t components;
+    if (info.ndim < 2 || info.ndim > 3)
+        throw std::runtime_error("Incompatible buffer dimensions (expected 2 or 3)");
+    if (info.ndim == 2)
+        components = 1;
+    else
+        components = info.shape[2];
+
+    if (components > 4)
+        throw std::runtime_error("Too many components (expected maximum 4)");
+
+    // Get the resulting data format
+    auto dataFormat = DataFormatBase::get(type, components, info.itemsize * 8);
+    if (!dataFormat)
+        throw std::runtime_error("Data format not supported");
+
+    // Create the layer RAM representation
     std::shared_ptr<LayerRAM> layerRAM;
-    if (info.format == py::format_descriptor<float>::format()) {
-        layerRAM = createLayerRAM(size2_t(info.shape[1], info.shape[0]), LayerType::Color, DataFloat32::get());
-    } else {
-        throw std::runtime_error("Incompatible format: " + info.format);
-    }
+    layerRAM = createLayerRAM(size2_t(info.shape[1], info.shape[0]), LayerType::Color, dataFormat);
+    if (!layerRAM)
+        throw std::runtime_error("Cannot allocate layer buffer");
 
     auto layerData = layerRAM->getData();
     memcpy(layerData, info.ptr, info.itemsize*info.size);
